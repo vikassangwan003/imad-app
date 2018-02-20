60be4b04b6de7cf12c9841324086cf72423ba967
vikassangwan003 <vikassangwan003@gmail.com>
2018-02-20 17:09:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,8 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-1',function(req,res){
-    res.send('Article 1 requested and will be serve here');
-    
+      res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
 });
 
 app.get('/article-2',function(req,res){
